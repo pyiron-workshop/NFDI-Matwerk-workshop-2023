f32f5a7b152c5badd22a1666c04452f92c9074d3
Sarath Menon <sarathmenon@mailbox.org>
2023-09-14 10:46:29 +0200

    remove contents

--- dgm_workshop/solution_3.py
+++
@@ -1,5 +0,0 @@
-for i in range(2, 9):
-    job=pr.create_job(job_type=YoungsModulusJob, job_name=f'y{i}', delete_existing_job=True)
-    job.input.filename = f'tensile_test/dataset_{i}.csv'
-    job.input.area = 120.636
-    job.run()
